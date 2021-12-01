13f37cf793658adc8463ae7fce380e8df69fe34e
Emre Pekkaya <emrepekkaya@gmail.com>
2021-12-01 18:10:24 +0300

    list Faculties

--- src/main/java/com/bilgeadam/bootcamp/services/FacultyService.java
+++ src/main/java/com/bilgeadam/bootcamp/services/FacultyService.java
@@ -3,6 +3,9 @@ package com.bilgeadam.bootcamp.services;
 import com.bilgeadam.bootcamp.payload.request.FacultyRequest;
 import com.bilgeadam.bootcamp.payload.response.FacultyResponse;
 
+import java.util.List;
+
 public interface FacultyService {
     FacultyResponse addFaculty(FacultyRequest facultyRequest);
+    List<FacultyResponse> listFaculties();
 }
