d25de527cee5243fa5a8b0f634f07da432839bde
Emre Pekkaya <emrepekkaya@gmail.com>
2021-12-04 15:26:01 +0300

    add README.md

--- src/main/java/com/bilgeadam/bootcamp/services/UserServiceImpl.java
+++ src/main/java/com/bilgeadam/bootcamp/services/UserServiceImpl.java
@@ -1,11 +1,7 @@
 package com.bilgeadam.bootcamp.services;
 
-import com.bilgeadam.bootcamp.models.Faculty;
 import com.bilgeadam.bootcamp.models.User;
-import com.bilgeadam.bootcamp.payload.request.FacultyRequest;
-import com.bilgeadam.bootcamp.payload.response.FacultyResponse;
 import com.bilgeadam.bootcamp.payload.response.UserResponse;
-import com.bilgeadam.bootcamp.repository.FacultyRepository;
 import com.bilgeadam.bootcamp.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
